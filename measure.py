c2abb9f37cbf175f6715bd4fc1f39a8e57eaf0f3
Dylan Smith <dysm8245@colorado.edu>
2023-08-18 15:36:38 -0600

    updated module functions

--- measure.py
+++ measure.py
@@ -1,18 +1,7 @@
 from math import pi as p
 
-def circumference():
-    try:
-        diameter = float(input("What is the diameter of the circle? Enter the number with units of feet.\n"))
-        circ = diameter*p
-        print(f"The circumference of a circle with a diameter of {diameter} feet is {circ} feet\n")
-    except:
-        print("You must enter a number value\n")
+def circumference(diameter):
+    return diameter*p
 
-def houseArea():
-    try:
-        length = float(input("What is the length of your house? Enter the number with units of feet.\n"))
-        width = float(input("What is the width of your house? Enter the number with units of feet.\n"))
-        area = length*width
-        print(f"The square footage of your house is {area} square feet\n")
-    except:
-        print("You must enter a number value\n")
+def houseArea(length, width):
+    return length*width
